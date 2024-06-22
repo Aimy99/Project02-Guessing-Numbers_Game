1daf4521ef4cd77ffb4518b8bc1e80fe35bea4da
Aimy99 <163441755+Aimy99@users.noreply.github.com>
2024-06-23 00:28:17 +0500

    Pushed CLI Guessing Game to GitHub.

---
+++ index.ts
@@ -0,0 +1,18 @@
+#! usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 100 + 1 ); 
+const answer = await inquirer.prompt([
+  {
+    message: "Please guess and enter a number between 1-100",
+    type: "number",
+    name: "GuessedNumberofUser",
+  },
+]);
+
+if (answer.GuessedNumberofUser === randomNumber ) {
+    console.log("You guessed the right number. Congrats!");
+} else { 
+    console.log("You guessed the wrong number. Let's Try Again!")
+};
